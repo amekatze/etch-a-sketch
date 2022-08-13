4898f159f500f3296d2ba3cda7a78ae178cdbc9f
amekatze <setharie@gmail.com>
2022-08-14 02:07:37 +0900

    Add toggle to mouseover draw upon click evevent

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let grid = 16;
 let color = 'standard';
 let pixelNow;
+let draw = false;
 const canvas = document.getElementById('canvas');
 createCanvas();
 
@@ -14,31 +15,33 @@ canvas.innerHTML = "";
     }
 eventRefresh()
 canvas.style.setProperty('grid-template-columns', `repeat(${grid}, 1fr)`);
+console.log(draw);
 }
 
+canvas.addEventListener('mousedown', () => {
+    draw = (draw == true ? false : true);
+    console.log(draw);});
 
 function eventRefresh(){
     const pixels = document.querySelectorAll('.pixel')
     pixels.forEach(pixel => {
     pixel.addEventListener('mouseover', () =>{
+    if (draw == true){
     pixelNow = pixel;
-    pixel.style.setProperty('background-color', colorPick());
-   })
-})
-}
+    pixel.style.setProperty('background-color', colorPick());}
+})})}
 
     
 function colorPick(){
     let rgb;
     if (color == 'standard'){
-    console.log(pixelNow.style.getPropertyValue('background-color'));
     let pixelColor = pixelNow.style.getPropertyValue('background-color');
-    
-    rgb = pixelColor == 'rgb(255, 255, 255)' ? 'rgb(192, 192, 192)' : 
-    pixelColor == 'rgb(192, 192, 192)' ? 'rgb(169, 169, 169)' : 
-    pixelColor == 'rgb(169, 169, 169)' ? 'rgb(128, 128, 128)' : 
-    pixelColor == 'rgb(128, 128, 128)' ? 'rgb(105, 105, 105)' : 
-    pixelColor == 'rgb(105, 105, 105)' || pixelColor == 'rgb(76, 76, 76)' ? 'rgb(76, 76, 76)' : 'rgb(192, 192, 192)';
+    rgb = pixelColor == 'rgb(255, 255, 255)' ? 
+            'rgb(192, 192, 192)' : 
+            pixelColor == 'rgb(192, 192, 192)' ? 'rgb(169, 169, 169)' : 
+            pixelColor == 'rgb(169, 169, 169)' ? 'rgb(128, 128, 128)' : 
+            pixelColor == 'rgb(128, 128, 128)' ? 'rgb(105, 105, 105)' : 
+            pixelColor == 'rgb(105, 105, 105)' || pixelColor == 'rgb(76, 76, 76)' ? 'rgb(76, 76, 76)' : 'rgb(192, 192, 192)';
     } else if (color == 'random'){rgb = `rgb(${(Array.from({length: 3}, () => Math.floor(Math.random() * 255)))})`
     } else if (color == 'black'){
     rgb = 'rgb(76, 76, 76)';
@@ -63,3 +66,4 @@ document.getElementById('black').onclick = function(){
         color = 'black';};
 document.getElementById('erase').onclick = function(){
             color = 'white';};
+
